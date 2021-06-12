0a795d24c60873c191b25d0a4590a56161694087
mustafa <mustafa@mQuBits.com>
2021-06-12 23:03:22 +0200

    Init test

---
+++ src/test/java/com/mqubits/customers/services/CustomerServiceTest.java
@@ -0,0 +1,16 @@
+package com.mqubits.customers.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @Test
+    void createEmployer() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+}
